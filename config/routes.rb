70bc7a11a6b9f68ac30d0c3fc2eabea32698119c
Kevin Tolby <katolby@gmail.com>
2020-01-31 10:15:52 -0800

    Dashboard/home page added

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
   get 'static_pages/home'
+  get 'static_pages/dashboard'
 
   get '/about', to: 'static_pages#about'
+  get '/dashboard', to: 'static_pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
