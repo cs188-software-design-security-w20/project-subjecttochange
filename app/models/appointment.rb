6418c72412e10494fa3288fc30faf54a434c4b0b
Lauren Fromm <laurenfromm@gmail.com>
2020-02-02 09:58:28 -0800

    Create Calendar component and add appointment page

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,28 @@
 class Appointment < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :patient_email, :practice_email, :appt_start, :duration, presence:true
+  validates :description
+before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @appointment = current_user.appointment.build(appointment_params)
+    if @appointment.save
+      flash[:success] = "Appointment created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def micropost_params
+      params.require(:appointment).permit(:content)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   devise_for :practices, path: 'practices', controllers: { sessions: 'practices/sessions', registrations: 'practices/registrations' }
   get 'static_pages/about'
   get 'static_pages/home'
+  get 'static_pages/newappointment'
 
   get '/about', to: 'static_pages#about'
+  get '/calendar', to: 'calendar#calendar'
+  get '/newappointment', to: 'static_pages#newappointment'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
